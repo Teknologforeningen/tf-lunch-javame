fff0df16c2a79d1b3ad4f0bef9f59368815c7b01
Oskar Ehnstrom <oskar.e@gmail.com>
2012-08-14 10:30:55 +0300

    Added license

--- src/fi/teknolog/app/s40/TFLunchApp.java
+++ src/fi/teknolog/app/s40/TFLunchApp.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2012 Oskar Ehnström
+ * Copyright (C) 2012 Teknologforeningen
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
